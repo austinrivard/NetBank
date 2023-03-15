ee78b78079cccdb872f531091b40ec8db0c70e40
austin <56987226+austinrivard@users.noreply.github.com>
2023-03-14 22:17:00 -0700

    initial commit

---
+++ netbank/src/main/java/edu/sjsu/cs160/team2/netbank/NetbankApplication.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cs160.team2.netbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetbankApplication.class, args);
+	}
+
+}
---
+++ netbank/src/test/java/edu/sjsu/cs160/team2/netbank/NetbankApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cs160.team2.netbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
