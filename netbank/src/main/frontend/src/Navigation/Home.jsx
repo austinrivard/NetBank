4533ee37629f6bf95a0b2e7500a6f1a49ecc8934
L3tran <vivsletran@gmail.com>
2023-04-05 21:15:57 -0700

    Front End Changes

---
+++ netbank/src/main/frontend/src/Navigation/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+/*
+const Home = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginFrom/>}/>
+                <Route path="/register" element={<RegisterFrom/>} />
+                <Route path="/atm" element={<Atm/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Home;
+*/
+const Home =() =>{
+    return(
+        <>This is the Home Page</>
+    )
+}
+export default Home;
