7420a2e04aff382f3faf725914a430cbac7d4a0a
austin <56987226+austinrivard@users.noreply.github.com>
2023-04-19 15:53:14 -0700

    fixed couple of bugs

--- netbank/src/main/java/edu/sjsu/cs160/team2/netbank/models/Account.java
+++ netbank/src/main/java/edu/sjsu/cs160/team2/netbank/models/Account.java
@@ -12,7 +12,7 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(length = 12)
-    private Integer accountNumber;
+    private String accountNumber;
 
     private Integer dollars;
 
